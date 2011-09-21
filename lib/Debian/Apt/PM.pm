65c80771c09aaea167b602a86a6ed05eb877bd19
Jozef Kutej <jozef@kutej.net>
2011-09-21 14:51:52 +0000

    Import of JKUTEJ/Debian-Apt-PM-0.08 from CPAN.
    gitpan-cpan-distribution: Debian-Apt-PM
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         JKUTEJ/Debian-Apt-PM-0.08.tar.gz
    gitpan-cpan-author:       JKUTEJ
    gitpan-cpan-maturity:     released

--- lib/Debian/Apt/PM.pm
+++ lib/Debian/Apt/PM.pm
@@ -3,7 +3,7 @@ package Debian::Apt::PM;
 use warnings;
 use strict;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use 5.010;
 
@@ -11,7 +11,7 @@ use Moose;
 use IO::Uncompress::Bunzip2 qw(bunzip2 $Bunzip2Error) ;
 use IO::Any;
 use Parse::Deb::Control 0.03;
-use Debian::Dpkg::Version 'version_compare';
+use Dpkg::Version 'version_compare';
 use AptPkg::Config '$_config';
 use LWP::Simple 'mirror', 'RC_OK';
 use Carp 'croak';
@@ -277,7 +277,7 @@ Debian::Apt::PM - locate Perl Modules in Debian repositories
 
 =head1 NOTE
 
-EXPERIMENTAL => "use at your own risk"; B<< # you have bin warned >>
+Needs following extra Debian packages C<libdpkg-perl> and C<libapt-pkg-perl>.
 
 =head1 SYNOPSIS
 
